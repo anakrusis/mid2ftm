f86046888cc3416b8c58bc3f13cb825f569712d7
anakrusis <howtofamitracker@gmail.com>
2020-05-25 19:50:30 -0400

    Added note event writing to .ftm

--- mid2ftm.py
+++ mid2ftm.py
@@ -1,31 +1,110 @@
 from mido import *
 from mido import MidiFile
 import sys
+import math
 
 path_in = sys.argv[1]
 path_out = sys.argv[2]
 
 midi_in = MidiFile(path_in)
+ftm_out = open(path_out, "wb")
+
+patterns = [] # stores all pattern data
 
 ROW_HIGHLIGHT_1 = 4
 ROW_HIGHLIGHT_2 = 20
 
-# Writing a blank .ftm for now
-ftm_out = open(path_out, "wb")
-ftm_out.write("FamiTracker Module".encode("ascii"))
-ftm_out.write(b"\x40\x04\x00\x00") # Version number (0.4.3+)
+def main():
+   
+    writeHdrAndParams(ftm_out)
 
-ftm_out.write("PARAMS".encode("ascii")) # Param block title
-ftm_out.write(b"\x00" * 10)
-ftm_out.write(b"\x06\x00\x00\x00") # Param block version (6)
-ftm_out.write(b"\x1d\x00\x00\x00") # Param block size
-ftm_out.write(b"\x00")             # Expansion chips disabled
-ftm_out.write(b"\x05\x00\x00\x00") # Number of channels (2a03 = 5ch)
-ftm_out.write(b"\x00\x00\x00\x00") # PAL mode disabled
-ftm_out.write(b"\x00\x00\x00\x00") # Default engine speed
-ftm_out.write(b"\x01\x00\x00\x00") # New vibrato mode enabled
+    currentPattern = 0
+    patterns.append( newPattern(0,0,currentPattern) )
+    currentTime = 0
 
-ftm_out.write((ROW_HIGHLIGHT_1).to_bytes(4, byteorder='little', signed=False))
-ftm_out.write((ROW_HIGHLIGHT_2).to_bytes(4, byteorder='little', signed=False))
+    track = midi_in.tracks[1]
+    print('Track {}: {}'.format(1, track.name))
+    for msg in track:
+        print(msg)
+        if msg.type == 'note_on':
+            patterns[currentPattern][3] = addToBytes( patterns[currentPattern][3], 4, 1)
+            patterns[currentPattern].extend( midiNoteToRow(msg.note, msg.velocity, currentTime) )
+            currentTime += 1
+            if currentTime >= 64:
+                currentPattern += 1
+                patterns.append( newPattern(0,0,currentPattern) )
+                currentTime = 0;
+    
+    ftm_out.write("FRAMES".encode("ascii")) # Frames block title
+    ftm_out.write(b"\x00" * 10)
+    ftm_out.write(b"\x03\x00\x00\x00") # Frames block version (3)
+    ftm_out.write( ( 16 + (len(patterns) * 5) ).to_bytes(4, byteorder='little', signed=False) ) # Frames block size
 
-ftm_out.write(b"\x20\x00\x00\x00") # Speed/tempo split
+    ftm_out.write(( len(patterns) ).to_bytes(4, byteorder='little', signed=False)) # number of frames
+    ftm_out.write((2).to_bytes(4, byteorder='little', signed=False)) # speed
+    ftm_out.write((150).to_bytes(4, byteorder='little', signed=False)) # tempo
+    ftm_out.write((64).to_bytes(4, byteorder='little', signed=False)) # rows per frame
+
+    for i in range(0, len(patterns)):
+        for j in range(0, 5):
+            ftm_out.write(( i ).to_bytes(1, byteorder='little', signed=False)) # pattern
+
+    writePatterns()
+
+def writePatterns():
+    ftm_out.write("PATTERNS".encode("ascii")) # Pattern block title
+    ftm_out.write(b"\x00" * 8)
+    ftm_out.write(b"\x05\x00\x00\x00") # Pattern block version (5)
+    ftm_out.write( (patternsLength()).to_bytes(4, byteorder="little", signed=False) ) # Pattern block size
+
+    for pattern in patterns:
+        for bytelist in pattern:
+            print(bytelist)
+            ftm_out.write(bytelist)
+    
+
+def addToBytes(byteArray, size, amt):
+    temp = int.from_bytes(byteArray, byteorder="little", signed=False) + amt
+    return temp.to_bytes(size, byteorder="little", signed=False)
+
+def patternsLength():
+    totalLength = 0;
+    for pattern in patterns:
+        for byteArray in pattern:
+            totalLength += len(byteArray)
+    return totalLength
+
+def newPattern(songIndex, channel, index):
+    return [ songIndex.to_bytes(4, byteorder='little', signed=False),
+             channel.to_bytes(4, byteorder='little', signed=False),
+             index.to_bytes(4, byteorder='little', signed=False),
+             (0).to_bytes(4, byteorder='little', signed=False) # defined rows amount (0 at the start)
+            ]
+
+def midiNoteToRow(note, velocity, rowIndex):
+    notename = (note % 12) + 1
+    octave = math.floor(note / 12) - 2
+
+    return [ rowIndex.to_bytes(4, byteorder='little', signed=False),
+             notename.to_bytes(1, byteorder='little', signed=False),
+             octave.to_bytes(1, byteorder='little', signed=False),
+             b"\x00\x10\x00\x00" ] # 0th instrument, no volume, no effects
+
+def writeHdrAndParams(f):
+    f.write("FamiTracker Module".encode("ascii"))
+    f.write(b"\x40\x04\x00\x00") # Version number (0.4.3+)
+
+    f.write("PARAMS".encode("ascii")) # Param block title
+    f.write(b"\x00" * 10)
+    f.write(b"\x06\x00\x00\x00") # Param block version (6)
+    f.write(b"\x1d\x00\x00\x00") # Param block size
+    f.write(b"\x00")             # Expansion chips disabled
+    f.write(b"\x05\x00\x00\x00") # Number of channels (2a03 = 5ch)
+    f.write(b"\x00\x00\x00\x00") # PAL mode disabled
+    f.write(b"\x00\x00\x00\x00") # Default engine speed
+    f.write(b"\x01\x00\x00\x00") # New vibrato mode enabled
+    f.write((ROW_HIGHLIGHT_1).to_bytes(4, byteorder='little', signed=False))
+    f.write((ROW_HIGHLIGHT_2).to_bytes(4, byteorder='little', signed=False))
+    f.write(b"\x20\x00\x00\x00") # Speed/tempo split
+
+main()
